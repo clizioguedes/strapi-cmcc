ae2753c5c097a69a7650840f5ffbea9bd03411d1
Pedro Henrique <pedro.lg.cs@gmail.com>
2025-06-23 09:28:40 -0300

    feat: create expenses

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,11 +2,19 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000', 'https://example.com', 'https://subdomain.example.com', 'https://someotherwebsite.org'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,9 @@
-export default () => ({});
+export default () => ({
+  upload: {
+    config: {
+      providerOptions: {},
+      sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
+      mimeTypes: [],
+    },
+  },
+});
